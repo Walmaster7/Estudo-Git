ef1982cfa66558ca6c21c36f6835cfaf1ec4275d
Walmaster7 <walter_machado@outlook.com.br>
2022-02-16 17:35:19 -0300

    Enviando os arquivos comitados

---
+++ Continuo_testando.js
@@ -0,0 +1,1 @@
+console.log("Continuo testando");
---
+++ Media_alunos.js
@@ -0,0 +1,12 @@
+prompt ("Digite o nome do aluno:    ");
+    var nome_aluno = prompt;
+prompt ("Digite a nota 1:    ");
+    var nota_1 = prompt;
+prompt ("Digite a nota 2:    ");
+    var nota_2 = prompt;
+    var media_total= (nota_1 + nota_2)/2; 
+    if (media_total => 7){ console.log( nome_aluno, "     Foi Aprovado");
+} else {console.log( "Aluno foi reprovado");
+}
+    
+    
